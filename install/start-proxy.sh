ccb35fe33b393e33fe50e5c7afc2887de8c477bc
Laurent Broudoux <laurent.broudoux@gmail.com>
2026-03-30 10:58:16 +0200

    chore: #42 Lower prod logs to INFO Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- install/start-proxy.sh
+++ install/start-proxy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker run -it --rm -p 7777:7777 -e RESHAPR_CTRL_HOST=host.docker.internal \
+  -e RESHAPR_CTRL_PORT=5555 \
+  -e RESHAPR_CTRL_TOKEN=reshapr-my-super-secret-token-xyz \
   --add-host=host.docker.internal:host-gateway \
   quay.io/reshapr/reshapr-proxy:nightly
